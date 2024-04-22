e711620c375f33a6bcaec5f02b86e499846f8cc1
etctera <yilonglei790@gmail.com>
2024-04-22 14:02:03 +0800

    feat: 错误处理

--- 01_ecli/src/main.rs
+++ 01_ecli/src/main.rs
@@ -1,3 +1,17 @@
+use std::{fs, io::Error, path::Path};
+
 fn main() {
-    println!("Hello, world!");
+    let content = read_file("assets/juventus.csv");
+
+    match content {
+        Ok(c) => println!("Content: {}", c),
+        Err(e) => println!("Error: {}", e),
+    }
+}
+
+fn read_file<P>(file_path: P) -> Result<String, Error>
+where
+    P: AsRef<Path>,
+{
+    fs::read_to_string(file_path)
 }
