65043c88ebb2c1d01c068c968f7327d8ae83c5ab
etctera <yilonglei790@gmail.com>
2024-04-30 11:22:40 +0800

    chore: remove comment

--- 01_ecli/src/process/csv_convert.rs
+++ 01_ecli/src/process/csv_convert.rs
@@ -7,17 +7,12 @@ use crate::opts::OutputFormat;
 
 pub fn process_csv(input: &str, output: String, format: OutputFormat) -> Result<()> {
     let mut rdr = Reader::from_path(input)?;
-    // 因为下面 rdr.records() 是 mutable borrow, 所以这里需要 clone rdr.header()? 这个 mutable borrow
     let headers = rdr.headers()?.clone();
     let mut values = HashMap::new();
 
     for res in rdr.records() {
         let record = res?;
 
-        // headers.iter() -> 使用 headers 的迭代器
-        // record.iter() -> 使用 record 的迭代器
-        // zip() -> 将两个迭代器合并为一个元组的迭代器 [(header, record), ..]
-        // collect::<Value>() -> 将元组的迭代器转换为 Value
         let value = headers
             .iter()
             .zip(record.iter())
